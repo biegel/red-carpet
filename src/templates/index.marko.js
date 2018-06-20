898bc1eafa57b00a30f0b0f1813660d430c3a246
tom biegeleisen pi@biegel.com <pi@biegel.com>
2018-06-20 03:52:11 +0000

    tweaking a few things for the pi

--- src/templates/index.marko.js
+++ src/templates/index.marko.js
@@ -1,17 +1,17 @@
 // Compiled using marko@4.10.0 - DO NOT EDIT
 "use strict";
 
-var marko_template = module.exports = require("marko/src/html").t(__filename),
+var marko_template = module.exports = require("marko/dist/html").t(__filename),
     marko_componentType = "/red-carpet$1.0.0/src/templates/index.marko",
-    components_helpers = require("marko/src/components/helpers"),
+    components_helpers = require("marko/dist/components/helpers"),
     marko_renderer = components_helpers.r,
     marko_defineComponent = components_helpers.c,
-    marko_helpers = require("marko/src/runtime/html/helpers"),
+    marko_helpers = require("marko/dist/runtime/html/helpers"),
     marko_escapeXmlAttr = marko_helpers.xa,
     marko_loadTag = marko_helpers.t,
-    component_globals_tag = marko_loadTag(require("marko/src/components/taglib/component-globals-tag")),
-    init_components_tag = marko_loadTag(require("marko/src/components/taglib/init-components-tag")),
-    await_reorderer_tag = marko_loadTag(require("marko/src/taglibs/async/await-reorderer-tag"));
+    component_globals_tag = marko_loadTag(require("marko/dist/components/taglib/component-globals-tag")),
+    init_components_tag = marko_loadTag(require("marko/dist/components/taglib/init-components-tag")),
+    await_reorderer_tag = marko_loadTag(require("marko/dist/taglibs/async/await-reorderer-tag"));
 
 function render(input, out, __component, component, state) {
   var data = input;
@@ -34,8 +34,8 @@ function render(input, out, __component, component, state) {
 }
 
 marko_template._ = marko_renderer(render, {
-    ___implicit: true,
-    ___type: marko_componentType
+    ae_: true,
+    _l_: marko_componentType
   });
 
 marko_template.Component = marko_defineComponent({}, marko_template._);
@@ -43,8 +43,8 @@ marko_template.Component = marko_defineComponent({}, marko_template._);
 marko_template.meta = {
     id: "/red-carpet$1.0.0/src/templates/index.marko",
     tags: [
-      "marko/src/components/taglib/component-globals-tag",
-      "marko/src/components/taglib/init-components-tag",
-      "marko/src/taglibs/async/await-reorderer-tag"
+      "marko/dist/components/taglib/component-globals-tag",
+      "marko/dist/components/taglib/init-components-tag",
+      "marko/dist/taglibs/async/await-reorderer-tag"
     ]
   };
